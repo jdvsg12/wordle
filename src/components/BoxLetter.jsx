c067b7bbba7cc8f88a59d246557d05178be48172
Julian Velandia <julianvelandia@quadi.co>
2023-11-06 22:15:42 -0500

    add event delete letter

--- src/components/BoxLetter.jsx
+++ src/components/BoxLetter.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react"
 import Keyboard from "../components/Keyboard";
 import { WORDS_LIST } from "../services/word"
+const maxNumberWord = "5";
 
 
 
 function BoxLetter() {
-  const wordShow = [];
   const [letterPress, setLetterPress] = useState([]);
   const [words, setWords] = useState([...WORDS_LIST]);
+  const [classStyle, setClassStyle] = useState([])
   const [wordRandom, setWordRandom] = useState(words[0]);
+  const [wordShow, setWordShow] = useState([])
 
   useEffect(() => {
     const randomNumber = Math.floor(Math.random() * words.length)
@@ -17,41 +19,48 @@ function BoxLetter() {
   }, [])
 
   function handleKeySelect(pressedKey) {
-    const maxNumberWord = "5";
     const letter = pressedKey.target.innerText;
     if (letterPress.length <= maxNumberWord) {
       setLetterPress(letterPress => [...letterPress, ...letter])
     }
   }
 
-  function handleDeleteLetter(deleteKey) {
-    letterPress.pop()
-  }
-
-  for (let i = 0; i < wordRandom.split("").length; i++) {
-    if (i < wordRandom.split("").length) {
-      const letter = wordRandom.split("")[i];
-      const valor = letterPress[i];
-      const objeto = { letter: letter, letterPress: valor };
-      wordShow.push(objeto)
+  function handleDeleteLetter() {
+    if (letterPress.length > 0) {
+      setLetterPress((prevLetterPress) => prevLetterPress.slice(0, -1));
     }
   }
+  useEffect(() => {
+    const newWordShow = [];
+    for (let i = 0; i < maxNumberWord; i++) {
+      if (i < maxNumberWord) {
+        const letter = wordRandom.split("")[i];
+        const letterPressValue = letterPress[i];
+        const classStyleValue = classStyle[i];
+        const objeto = { letter: letter, classStyle: classStyleValue, letterPress: letterPressValue };
+        newWordShow.push(objeto);
+      }
+    }
+    setWordShow(newWordShow);
+  }, [wordRandom, letterPress]);
 
-  function handleValidator(e) {
-
+  const handleValidator = () => {
+    if (letterPress.length >= maxNumberWord) {
+      console.log("funciona")
+    }
   }
 
 
   return (
     <section>
       <article className="w-568 m-auto flex justify-between items-center my-10">
-        {wordShow.map(({ letter, letterPress }, index) => {
+        {wordShow.map(({ letter, classStyle, letterPress }, index) => {
           return (
-            <div onChange={handleValidator} className="letters" key={`${letter}-${index}`}>{letterPress}</div>
+            <div className={!classStyle ? "letters" : classStyle} key={`${letter}-${index}`}>{letterPress}</div>
           )
         })}
       </article>
-      <Keyboard handleKeySelect={handleKeySelect} handleDeleteLetter={handleDeleteLetter} handleValidator={handleValidator} />
+      <Keyboard wordShow={wordShow} handleKeySelect={handleKeySelect} handleDeleteLetter={handleDeleteLetter} handleValidator={handleValidator} />
     </section>
   )
 }
